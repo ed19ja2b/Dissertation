32d5303aa31820f1d9e94dc5e7fae9e569254562
Joshua Arlott-Beal <ed19ja2b@leeds.ac.uk>
2023-02-22 12:49:00 +0000

    Refactored code - can tweak the inner workings without affecting rest of code. De-randomised water colour, now uses Lerp to determine the water colour based on the water depth of a cell. Depth is currently based on distance from center and an arbitrary 'maxDepth' - *not* permanent. Can now update grid size / cell size in real time and see affect on grid. Next steps is to develop solver.

--- Assets/Scripts/InstantiateGridCells.cs
+++ Assets/Scripts/InstantiateGridCells.cs
@@ -1,50 +1,85 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System;
 using UnityEngine;
 
 public class InstantiateGridCells : MonoBehaviour
 {
+	const float maxDepth = 30.0f;//in meters (45ft/13.7m average ish)- find solid source for this
+
+	private int _gridSize;
+	private int _cellSize;
 	public int gridSize;
 	public int cellSize;//units
 
 	public GameObject waterCellPrefab;
 	public GameObject rockCellPrefab;
 
-	void Start()
-	{
-			cellSize = 1;
-			gridSize = 50;
+	void AdjustCamera(){
+		//adjusting camera to view entire grid
+		int orthographicSize = (gridSize * cellSize)/2;
+		float offset = 0.5f;
 
-			//adjusting camera to view entire grid
-			Camera.main.orthographicSize = (gridSize * cellSize) / 2;//assuming always square aspect ratio (1/1 = 1)
-			Camera.main.transform.position = new Vector3((gridSize * cellSize)/2 - 0.5f,(gridSize * cellSize)/2 - 0.5f, -10);//subtracts offset to centre grid
+		Camera.main.orthographicSize = orthographicSize;//(/1) - assuming always square aspect ratio (1/1 = 1)
+		Camera.main.transform.position = new Vector3(orthographicSize - offset, orthographicSize - offset, -10);//subtracts offset to centre grid
+	}
+
+	int IsRockCell(int x, int y){
+			float distanceFromCenter = Vector2.Distance(new Vector2(x, y), new Vector2(gridSize/2,gridSize/2));
+			if (distanceFromCenter < gridSize/4){//if within a central circle, instantiate as a rock cell
+					return 0;
+			}
+			return -1;
+	}
+
+	void InstantiateGrid(){
+			AdjustCamera();
 
 			//instantiating cells
 			for (int x = 0; x < gridSize; x++){
 					for (int y = 0; y < gridSize; y++){
-
 							//instantiating rock cells based on distance from center of grid
-							float distanceFromCenter = Vector2.Distance(new Vector2(x, y), new Vector2(gridSize/2,gridSize/2));
-							if (distanceFromCenter < gridSize/4){//if within a central circle, instantiate as a rock cell
+							if (IsRockCell(x,y) == 0){//if within a central circle, instantiate as a rock cell
 								GameObject cell = Instantiate(rockCellPrefab, transform);
 								cell.transform.position = new Vector3(x * cellSize, y * cellSize, 0);
 							}
 							else{//otherwise, instantiate water cell
-
 								GameObject cell = Instantiate(waterCellPrefab, transform);
 								cell.transform.position = new Vector3(x * cellSize, y * cellSize, 0);
 
 								//set water cell colour
-								// ************ - assign based on water cell depth
+								// ************ later - assign based on water cell depth
 								WaterCell waterCell = cell.GetComponent<WaterCell>();
 								if (waterCell != null){
-									Color randomBlue = Random.ColorHSV(241f / 360f, 260f / 360f, 1f, 1f, 0.6f, 1f);
-									waterCell.SetCellColor(randomBlue);
+									float distanceFromCenter = Vector2.Distance(new Vector2(x, y), new Vector2(gridSize/2,gridSize/2));
+									float maxDistance = (float)Math.Sqrt(2 * (Math.Pow((gridSize/2), 2)));
+									float waterDepth = (distanceFromCenter/maxDistance) * maxDepth;
+									waterCell.SetDepth(waterDepth);
 								}
-
-								
 							}
 					}
+			}
+	}
+
+	void Start(){
+			gridSize = 20;
+			cellSize = 1;
+			_gridSize = gridSize;
+			_cellSize = cellSize;
+			InstantiateGrid();
+	}
+
+	void Update()
+	{
+			if (gridSize != _gridSize || cellSize != _cellSize){
+					GameObject[] cells = GameObject.FindGameObjectsWithTag("WaterCell");
+					cells = cells.Concat(GameObject.FindGameObjectsWithTag("RockCell")).ToArray();
+					foreach(GameObject cell in cells)
+					GameObject.Destroy(cell);
+					InstantiateGrid();
+					_gridSize = gridSize;
+					_cellSize = cellSize;
 			}
 	}
 }
--- Assets/Scripts/WaterCell.cs
+++ Assets/Scripts/WaterCell.cs
@@ -9,9 +9,9 @@ public class WaterCell : MonoBehaviour
 		public Vector3 position;
 
 		const double g = 9.81;
+		const float maxDepth = 30.0f;
 
-		public double bed_elevation;
-		public double depth;
+		public float depth;
 		public double velocity_x;
 		public double velocity_y;
 		//bernoulli hydraulic head - can use to model how surface roughness affects water flow
@@ -21,13 +21,25 @@ public class WaterCell : MonoBehaviour
 				position = transform.position;
 		}
 
-		public void SetCellColor(Color color){
+		public void SetCellColor(){
+			// randomised
+			// Color color = UnityEngine.Random.ColorHSV(241f / 360f, 260f / 360f, 1f, 1f, 0.6f, 1f);
+			// Calculating colour based on the depth value
+      Color color = Color.Lerp(new Color(0, 0.26f, 0.51f), new Color(0, 0.5f, 1), 1 - (depth/maxDepth));
+			Debug.Log("Color value: " + color.ToString() + " depth/maxdepth: " + (depth/maxDepth).ToString());
+
 			spriteRenderer = GetComponent<SpriteRenderer>();
 			spriteRenderer.color = color;
 		}
 
+		public void SetDepth(float _depth){
+			 depth = _depth;
+			 SetCellColor();
+		}
+
 		public void CalculateBernoulli(){
 				//https://www.sciencedirect.com/science/article/pii/S0022169422010198 - 2.2 (1)
-				bernoulli_head = bed_elevation + depth + (Math.Pow(velocity_x,2) + Math.Pow(velocity_y,2)) / (2*g);
+				//taking bed elevation to be equal to water depth - not concerned about tidal heights, etc.
+				bernoulli_head = (2 * depth) + (Math.Pow(velocity_x,2) + Math.Pow(velocity_y,2)) / (2*g);
 		}
 }
